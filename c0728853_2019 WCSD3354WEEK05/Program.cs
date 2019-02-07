05545e7a82e0e86b8854405a6d3d3323db7b3c52
visualstudio <visualstudio@230-PC40>
2019-02-07 15:52:15 -0500

    Add project files.

---
+++ c0728853_2019 WCSD3354WEEK05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c0728853_2019_WCSD3354WEEK05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
