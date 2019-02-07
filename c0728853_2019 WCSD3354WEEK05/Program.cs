7033878e513b293201c1d735262dcb329637e919
visualstudio <visualstudio@230-PC40>
2019-02-07 16:55:25 -0500

    4:55

--- c0728853_2019 WCSD3354WEEK05/Program.cs
+++ c0728853_2019 WCSD3354WEEK05/Program.cs
@@ -4,103 +4,145 @@ namespace C0728853
 {
     class Program
     {
-        static void Main(string[] args)
         {
-            var a = new TestQuestion2();
-            //a.PlayingWithForLoops();
+class Program
+        {
+            static void Main(string[] args)
+            {
+                // var a = new testquestion();
+                // a.playingwithforloops();
+                birthday_party peter = new birthday_party();
+                peter.setupPartyList();
+                peter.reverseprintPartyList();
+                Console.WriteLine(peter.printPartyList());
+                Console.WriteLine(peter.reverseprintPartyList());
 
-            var b = new birthday_party();
-            b.setupPartyList();
-            Console.WriteLine(b.printPartyList());
+            }
+        }
+        /* class testquestion
+        {
+        public int myfavvariable = 0;
+        public void playingwithforloops()
+        {
+        for (; mymethod();)
+        {
+        if (myfavvariable > 10)
+        {
+        Console.WriteLine("I am outof the for loop");
+        break;
+        }
+        Console.WriteLine("oh no ihave to go through this stupid loop again");
+        }
+        }
+        public bool mymethod()
+        {
+        myfavvariable++;
+        return true;
+        }
+        }*/
+        class dog
+        {
+            public dog(string name, string breed)
+            {
+                dog_name = name;
+                dog_breed = breed;
+            }
+
+
+
+            public string dog_name;
+            public string dog_breed;
+            public dog next_dog;
+            public dog prev_dog;
 
         }
-    }
-
-    class TestQuestion2
-    {
-        public int myFavoriteVariable = 0;
-
-        public void PlayingWithForLoops()
+        class birthday_party
         {
-            // write a For Loop to Add 10 Numbers
-            while (MyMethod())
+            public dog peanut;
+            public dog fifi;
+            public dog clarence;
+            public dog gizelle;
+            public dog lulu;
+            public dog roy;
+
+
+            public dog head;
+            public dog tail;
+            public dog temporary;
+
+
+
+            public void setupPartyList()
             {
-                if (myFavoriteVariable > 10)
+                peanut = new dog("peanut", "bichon");
+                fifi = new dog("fifi", "poodle");
+                clarence = new dog("clarence", "german");
+                gizelle = new dog("gizelle", "buorder collie");
+                lulu = new dog("lulu", "shitzu");
+                roy = new dog("roy", "beagle");
+
+
+
+                peanut.prev_dog = null;
+                peanut.next_dog = fifi;
+
+                fifi.prev_dog = peanut;
+                fifi.next_dog = clarence;
+
+                clarence.prev_dog = fifi;
+                clarence.next_dog = gizelle;
+
+
+                gizelle.prev_dog = clarence;
+                gizelle.next_dog = lulu;
+
+
+                lulu.prev_dog = gizelle;
+                lulu.next_dog = roy;
+
+                roy.prev_dog = lulu;
+                roy.next_dog = null;
+                head = peanut;
+                tail = roy;
+
+
+
+            }
+            public string printPartyList()
+            {
+                string inviteList = "*--";
+                temporary = head;
+
+                while (temporary.next_dog != null)
                 {
-                    Console.WriteLine("i am so out of here!");
-                    break;
+
+
+                    inviteList += temporary.dog_name + " * --- * ";
+
+
+                    temporary = temporary.next_dog;
                 }
-                Console.WriteLine("oh no I have to go through this stupid loop again...");
-            }
-        }
-        public bool MyMethod()
-        {
-            myFavoriteVariable++;
-            return true;
-        }
-    }
-
-    class dog
-    {
-        public dog(string name, string breed)
-        {
-            dog_name = name;
-            dog_breed = breed;
-        }
-
-        public dog()
-        {
-        }
-
-        public string dog_name;
-        public string dog_breed;
-        public dog next_dog;
-        public dog prev_dog;
-
-    }
-
-    class birthday_party
-    {
-        public dog peanut;
-        public dog fifi;
-        public dog clarence;
-        public dog roy;
-
-        public dog head;
-        public dog tail;
-        public dog temporary;
-
-        public void setupPartyList()
-        {
-            peanut = new dog("Peanut", "Bichon");
-            fifi = new dog("Fifi", "Poodle");
-            clarence = new dog("Clarence", "German Sheppard");
-            roy = new dog("Roy", "Beagle");
-
-            peanut.prev_dog = null;
-            peanut.next_dog = fifi;
-            fifi.prev_dog = peanut;
-            fifi.next_dog = clarence;
-            clarence.prev_dog = fifi;
-            clarence.next_dog = roy;
-            roy.prev_dog = clarence;
-            roy.next_dog = null;
-            head = peanut;
-            tail = roy;
-        }
-
-        public string printPartyList()
-        {
-            string inviteList = "*--";
-            temporary = head;
-            while (temporary.next_dog != null)
-            {
                 inviteList += temporary.dog_name + " * --- * ";
-                temporary = temporary.next_dog;
+                return inviteList;
+
             }
+            public string reverseprintPartyList()
+            {
+                string inviteList = "*--";
+                temporary = tail;
 
-            return inviteList;
+                while (temporary.prev_dog != null)
+                {
+
+
+                    inviteList += temporary.dog_name + " * --- * ";
+
+
+                    temporary = temporary.prev_dog;
+                }
+                inviteList += temporary.dog_name + " * --- * ";
+                return inviteList;
+
+            }
         }
-
     }
-}
